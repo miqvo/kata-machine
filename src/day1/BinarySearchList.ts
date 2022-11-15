3e6ef7038ffaba389ff6490e589a95a2b38a266e
miqvo <vo8919@gmail.com>
2022-11-15 21:53:30 +0100

    .

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    let m = Math.floor(lo + (lo - hi) / 2);
+    let v = haystack[m];
+
+    while (lo < hi) {
+        if (v === needle) return true;
+        else if (v > needle) hi = m;
+        else lo = m + 1;
+    }
+
+    return false;
 }
